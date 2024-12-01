4eb5f6fcb8e7278a30696430db6c8a130add5902
PradeepDi <Pradeepdissanayaka.pm@gmail.com>
2024-12-01 12:44:59 +0530

    Database development

--- src/firebase.js
+++ src/firebase.js
@@ -44,6 +44,8 @@ export { auth, db };
 
 
 
+
+
 /*// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
